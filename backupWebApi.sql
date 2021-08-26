4e841e7dc4465d1958faa4d98ebe47122c96e6bc
DanielEscudeiro <dancaescudeiro@gmail.com>
2021-08-26 17:09:26 -0300

    Documento API e Backup

---
+++ backupWebApi.sql
@@ -0,0 +1,22 @@
+﻿
+-- DROP DATABASE testetecnico;
+
+CREATE DATABASE testetecnico;
+
+CREATE SCHEMA testetecnicoelotech;
+
+CREATE TABLE testetecnicoelotech.pessoa
+(
+  id bigserial NOT NULL PRIMARY KEY,
+  nome character varying(150),
+  cpf character varying(60),
+  datanascimento character varying(60)
+);
+
+Create Table testetecnicoelotech.contatos(
+  id bigserial NOT NULL PRIMARY KEY,
+  nome character varying(150),
+  telefone character varying(60),
+  email character varying(100),
+  id_pessoa bigserial references testetecnicoelotech.pessoa (id)
+);
